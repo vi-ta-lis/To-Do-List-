8af835b5463fb90ab94245aa2318d10bf87270b0
Vitalis <vitaliskalu7@gmail.com>
2024-08-20 12:40:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let addToButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+
+addToButton.addEventListener('click', function ()  {
+
+var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+
+
+    paragraph.addEventListener('click', function() {
+    paragraph.style.textDecoration = 'line-through';
+
+    paragraph.addEventListener('dblclick', function () {
+    paragraph.style.display = 'none';
+
+    })
+})})
+
+
